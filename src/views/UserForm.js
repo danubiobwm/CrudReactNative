061f18479c6c0a3fc07613aef9fe4c891b24bfb5
danubiobwm <danubio.bwm@gmail.com>
2020-10-11 01:11:01 -0300

    Finishe Redux/Hooks

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState, useContext} from 'react';
+import {Text, View, TextInput, StyleSheet, Button} from 'react-native';
+import UsersContext from '../context/UserContext';
 
-// import { Container } from './styles';
+export default ({route, navigation}) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
+  const {dispatch} = useContext(UsersContext);
 
-const UserForm = () => {
   return (
-    <View>
-    <Text>UserForm</Text>
+    <View style={style.form}>
+      <Text>Nome</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={(name) => setUser({...user, name})}
+        placeholder="Informe o Nome"
+        value={user.name}
+      />
+      <Text>Email</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={(email) => setUser({...user, email})}
+        placeholder="Informe o E-mail"
+        value={user.email}
+      />
+      <Text>URL do Avatar</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={(avatarUrl) => setUser({...user, avatarUrl})}
+        placeholder="Informe a URL do Avatar"
+        value={user.avatarUrl}
+      />
+      <Button
+        title="Salvar"
+        onPress={() => {
+          dispatch({
+            type: user.id ? 'updateUser' : 'createUser',
+            payload: user,
+          });
+          navigation.goBack();
+        }}
+      />
     </View>
   );
-}
+};
 
-export default UserForm;
+const style = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+  },
+});
